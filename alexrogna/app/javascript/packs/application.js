acabe653646b879d3644e23d72ffb7a30f132d5d
AlexUkPC <alexrogna@yahoo.com>
2021-05-22 15:10:56 +0100

    skills modals

--- alexrogna/app/javascript/packs/application.js
+++ alexrogna/app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("books.js")
-require("linkedin.js")
+import './modal'
 require("jquery")
 require("@nathanvda/cocoon")
 Rails.start()
