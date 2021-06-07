6ef6afb0f15c37466f64e0eb0d9bd9720334406f
AlexUkPC <alexrogna@yahoo.com>
2021-06-07 11:25:50 +0100

    add js navbar

--- alexrogna/public/js/main.js
+++ alexrogna/public/js/main.js
@@ -14,11 +14,51 @@
     }; // end ssParallax
 
 
-   
+   /* Move header menu */
+    const ssMoveHeader = function () {
+
+        const hdr = document.querySelector('.s-header');
+        const hero = document.querySelector('#hero');
+        let triggerHeight;
+
+        if (!(hdr && hero)) return;
+
+        setTimeout(function(){
+            triggerHeight = hero.offsetHeight - 170;
+        }, 300);
+
+        window.addEventListener('scroll', function () {
+
+            let loc = window.scrollY;
+        
+
+            if (loc > triggerHeight) {
+                hdr.classList.add('sticky');
+            } else {
+                hdr.classList.remove('sticky');
+            }
+
+            if (loc > triggerHeight + 20) {
+                hdr.classList.add('offset');
+            } else {
+                hdr.classList.remove('offset');
+            }
+
+            if (loc > triggerHeight + 150) {
+                hdr.classList.add('scrolling');
+            } else {
+                hdr.classList.remove('scrolling');
+            }
+
+        });
+
+    }; // end ssMoveHeader  
+
    /* initialize */
     (function ssInit() {
 
         ssParallax();
+        ssMoveHeader();
 
     })();
 
