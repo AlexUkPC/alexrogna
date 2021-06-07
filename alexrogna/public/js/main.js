599682fe9cf5c8546893ebde9eba90d805a0b8f8
AlexUkPC <alexrogna@yahoo.com>
2021-06-07 11:28:55 +0100

    add js projects lightbox

--- alexrogna/public/js/main.js
+++ alexrogna/public/js/main.js
@@ -128,6 +128,41 @@
         }
 
     }; // end ssScrollSpy
+
+
+    /* Lightbox Projects */
+    const ssLightboxProject = function() {
+
+        const projectLinks = document.querySelectorAll('.project-item a');
+        const modals = [];
+
+        projectLinks.forEach(function(link) {
+            let modalbox = link.getAttribute('href');
+            let instance = basicLightbox.create(
+                document.querySelector(modalbox),
+                {
+                    onShow: function(instance) {
+                        //detect Escape key press
+                        document.addEventListener("keydown", function(evt) {
+                            evt = evt || window.event;
+                            if(evt.keyCode === 27){
+                            instance.close();
+                            }
+                        });
+                    }
+                }
+            )
+            modals.push(instance);
+        });
+
+        projectLinks.forEach(function(link, index) {
+            link.addEventListener("click", function(e) {
+                e.preventDefault();
+                modals[index].show();
+            });
+        });
+
+    };  // end ssLightboxProject
    /* initialize */
     (function ssInit() {
 
@@ -135,6 +170,7 @@
         ssMoveHeader();
         ssMobileMenu();
         ssScrollSpy();
+        ssLightboxProject();
 
     })();
 
