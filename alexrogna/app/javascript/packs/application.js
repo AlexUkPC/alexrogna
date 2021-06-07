d0e76d49a8fd6ecc91336f97eb769dd3af41ab6b
AlexUkPC <alexrogna@yahoo.com>
2021-06-07 11:23:00 +0100

    add js plugins

--- alexrogna/app/javascript/packs/application.js
+++ alexrogna/app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import './modal'
-import './progress_bar'
 require("jquery")
 require("@nathanvda/cocoon")
 Rails.start()
