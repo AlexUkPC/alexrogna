12e02eed27057345251bc11ca249f6e0501874a2
AlexUkPC <alexrogna@yahoo.com>
2021-06-07 11:29:51 +0100

    add js skills lightbox

--- alexrogna/public/js/main.js
+++ alexrogna/public/js/main.js
@@ -163,6 +163,42 @@
         });
 
     };  // end ssLightboxProject
+    
+
+    /* Lightbox Skills */
+    const ssLightboxSkills = function() {
+
+        const skillLinks = document.querySelectorAll('.skill-item a');
+        const modals = [];
+
+        skillLinks.forEach(function(link) {
+            let modalbox = link.getAttribute('href');
+            let instance = basicLightbox.create(
+                document.querySelector(modalbox),
+                {
+                    onShow: function(instance) {
+                        //detect Escape key press
+                        document.addEventListener("keydown", function(evt) {
+                            evt = evt || window.event;
+                            if(evt.keyCode === 27){
+                            instance.close();
+                            }
+                        });
+                    }
+                }
+            )
+            modals.push(instance);
+        });
+
+        skillLinks.forEach(function(link, index) {
+            link.addEventListener("click", function(e) {
+                e.preventDefault();
+                modals[index].show();
+            });
+        });
+
+    };  // end ssLightboxSkills
+
    /* initialize */
     (function ssInit() {
 
@@ -171,6 +207,7 @@
         ssMobileMenu();
         ssScrollSpy();
         ssLightboxProject();
+        ssLightboxSkills();
 
     })();
 
