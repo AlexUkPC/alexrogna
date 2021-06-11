8874b5381646e2a57c22f986a59183177fbda1f4
AlexUkPC <alexrogna@yahoo.com>
2021-06-11 14:25:00 +0100

    skills - books

--- alexrogna/app/controllers/books_controller.rb
+++ alexrogna/app/controllers/books_controller.rb
@@ -64,6 +64,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :subtitle, :author, :publisher, :dop, :resources_link, :github_link)
+      params.require(:book).permit(:title, :subtitle, :author, :publisher, :dop, :resources_link, :github_link, :cover)
     end
 end
