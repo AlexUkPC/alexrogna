2f723b97e8016b176c392083a52148f9516d6c60
AlexUkPC <alexrogna@yahoo.com>
2021-06-27 17:04:17 +0100

    add education courses

--- alexrogna/app/models/education.rb
+++ alexrogna/app/models/education.rb
@@ -1,4 +1,5 @@
 class Education < ApplicationRecord
   has_and_belongs_to_many :teches
   accepts_nested_attributes_for :teches, reject_if: :all_blank, allow_destroy: true
+  has_rich_text :courses
 end
