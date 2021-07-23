27451ca2fd2b2ecd73f592336c88df82ea26bf76
AlexUkPC <alexrogna@yahoo.com>
2021-07-23 11:41:11 +0100

    change mail from test to contact

--- alexrogna/app/mailers/application_mailer.rb
+++ alexrogna/app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   helper HomeHelper, CvHelper, ApplicationHelper
-  default from: 'test@alexrogna.com'
+  default from: 'contact@alexrogna.com'
   layout 'mailer'
 end
